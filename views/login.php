5de2d9812afd57efda1e64f6a7fbd773bf685352
Nedas <nedas.michailovas@stud.svako.lt>
2026-05-17 22:25:02 +0300

    Pridėtas session_start() puslapio viršuje

--- views/login.php
+++ views/login.php
@@ -1,4 +1,33 @@
-<?php require_once '../classes/User.php'; ?>
+<?php
+// session_start() PIRMA, prieš bet kokį išvestį
+session_start();
+require_once '../classes/User.php';
+
+// Jei jau prisijungęs – nukreipiame
+if (isset($_SESSION['user_id'])) {
+    header("Location: dashboard.php");
+    exit;
+}
+
+$error = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = trim($_POST['username'] ?? '');
+    $password = $_POST['password'] ?? '';
+
+    if ($username && $password) {
+        $user = new User();
+        if ($user->login($username, $password)) {
+            header("Location: dashboard.php");
+            exit;
+        } else {
+            $error = 'Neteisingi prisijungimo duomenys!';
+        }
+    } else {
+        $error = 'Prašome užpildyti visus laukus.';
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="lt">
 <head>
@@ -8,23 +37,16 @@
 </head>
 <body>
     <h2>Prisijungimas</h2>
+
+    <?php if ($error): ?>
+        <p class="error"><?= htmlspecialchars($error) ?></p>
+    <?php endif; ?>
+
     <form method="POST">
         Vartotojo vardas: <input type="text" name="username" required><br><br>
         Slaptažodis: <input type="password" name="password" required><br><br>
         <button type="submit">Prisijungti</button>
     </form>
     <p><a href="register.php">Neturi paskyros? Registruokis</a></p>
-
-    <?php
-    if ($_POST) {
-        $user = new User();
-        if ($user->login($_POST['username'], $_POST['password'])) {
-            header("Location: dashboard.php");
-            exit;
-        } else {
-            echo "<p style='color:red'>Neteisingi prisijungimo duomenys!</p>";
-        }
-    }
-    ?>
 </body>
 </html>
