7319b065f1e8418c25d07db8dcd4ad70e5431106
Nedas <nedas.michailovas@stud.svako.lt>
2026-05-17 21:34:54 +0300

    Sukurtas add_password.php

---
+++ views/add_password.php
@@ -0,0 +1,31 @@
+<?php 
+session_start();
+if (!isset($_SESSION['user_id'])) header("Location: login.php");
+require_once '../classes/PasswordVault.php';
+
+if ($_POST) {
+    $vault = new PasswordVault();
+    $result = $vault->addPassword($_POST['site_name'], $_POST['password'], $_POST['notes'] ?? '');
+    if ($result) {
+        header("Location: vault.php");
+        exit;
+    } else {
+        echo "<p style='color:red'>Klaida išsaugant!</p>";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="lt">
+<head><meta charset="UTF-8"><title>Pridėti slaptažodį</title></head>
+<body>
+<h2>Pridėti naują slaptažodį</h2>
+<form method="POST">
+    Svetainė / Programa: <input type="text" name="site_name" required><br><br>
+    Slaptažodis: <input type="text" name="password" required><br><br>
+    Pastabos: <textarea name="notes"></textarea><br><br>
+    <button type="submit">Išsaugoti</button>
+</form>
+<br>
+<a href="vault.php">Grįžti į saugyklą</a>
+</body>
+</html>
